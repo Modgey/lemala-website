d44ed0608fddf987af08b3cf959745636b5ed619
Modgey <modgegaming@gmail.com>
2025-05-12 01:45:13 +0300

    [Cursor] Fix gradient mask with elliptical shape to prevent cutoff

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             cr={1.5} 
             className={cn(
               "text-black/40 -z-10",
-              "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]"
+              "[mask-image:radial-gradient(ellipse_1000px_700px_at_center,white,transparent)]"
             )}
           />
           
